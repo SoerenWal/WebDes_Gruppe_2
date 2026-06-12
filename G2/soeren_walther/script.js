85a027d30211b6b32ecff47047e92a97b27c8225
Sören Walther <soe.walther@outlook.de>
2026-06-12 14:48:12 +0200

    Make progress

---
+++ G2/soeren_walther/script.js
@@ -0,0 +1,13 @@
+const pickers = document.querySelectorAll(".colorpicker");
+const ausblick = document.getElementById("ausblick");
+
+pickers.forEach(picker => {
+    picker.addEventListener("click", () => {
+        // optional: nur ein aktiver gleichzeitig
+        pickers.forEach(p => p.classList.remove("active"));
+
+        // aktuelles aktivieren
+        picker.classList.add("active");
+        ausblick.src = picker.dataset.image;
+    });
+});
